9bfaef57d01e30c2b5f7b9ecf63ce134c9252886
Kellen Piffner <k@8labs.com>
2020-07-21 11:11:45 -0700

    sketching in some ideas

---
+++ ForkingVirtualMachine.Store/Models/FeedChange.cs
@@ -0,0 +1,15 @@
+﻿namespace ForkingVirtualMachine.Store.Models
+{
+    using System;
+    using System.Numerics;
+
+    public class FeedChange
+    {
+        public byte[] NodeId { get; set; }
+
+        // this is a stream: could be used instead of val on node table (immutable)
+        public BigInteger Value { get; set; }
+
+        public DateTime ModifiedOn { get; set; }
+    }
+}
---
+++ ForkingVirtualMachine/IScope.cs
@@ -0,0 +1,8 @@
+﻿namespace ForkingVirtualMachine
+{
+    interface IScope : IDescribe
+    {
+        public void Define(byte[] word, IVirtualMachine machine);
+        public void Call(byte[] scopeId, byte[] word, Context context);
+    }
+}
